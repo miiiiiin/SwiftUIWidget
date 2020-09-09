d9b4df0e90eff458923e240fc6acd06a995c029c
songkyung <min.songkyung@gmail.com>
2020-09-09 14:55:02 +0900

    first commit

---
+++ SwiftUIWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWidget
+//
+//  Created by Running Raccoon on 2020/09/09.
+//  Copyright © 2020 Songkyung Min. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
