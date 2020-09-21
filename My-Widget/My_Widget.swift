a4eeeeb8cd6cca47899908913cf39be5c5d5aaa2
songkyung <min.songkyung@gmail.com>
2020-09-21 11:57:57 +0900

    create widget provider on widgetExtension

--- My-Widget/My_Widget.swift
+++ My-Widget/My_Widget.swift
@@ -8,63 +8,62 @@
 
 import WidgetKit
 import SwiftUI
-import Intents
 
-struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+struct EmojiEntry: TimelineEntry {
+    let date = Date()
+    let emoji: Emoji
+}
+
+struct Provider: TimelineProvider {
+    
+    typealias Entry = EmojiEntry
+    
+    @AppStorage("emoji", store: UserDefaults(suiteName: "group.kr.miiiiiin.SwiftUIWidget"))
+    var emojiData: Data = Data()
+    
+    func placeholder(in context: Context) -> EmojiEntry {
+        guard let emoji = try? JSONDecoder().decode(Emoji.self, from: emojiData) else { return EmojiEntry(emoji: Emoji(icon: "😊", name: "N/A", description: "N/A")) }
+        let entry = EmojiEntry(emoji: emoji)
+        return entry
     }
-
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+    
+    func getSnapshot(in context: Context, completion: @escaping (EmojiEntry) -> Void) {
+        guard let emoji = try? JSONDecoder().decode(Emoji.self, from: emojiData) else { return }
+        let entry = EmojiEntry(emoji: emoji)
         completion(entry)
     }
-
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<EmojiEntry>) -> Void) {
+        guard let emoji = try? JSONDecoder().decode(Emoji.self, from: emojiData) else { return }
+        let entry = EmojiEntry(emoji: emoji)
+        let timeLine = Timeline(entries: [entry], policy: .never)
+        completion(timeLine)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationIntent
-}
-
-struct My_WidgetEntryView : View {
-    var entry: Provider.Entry
-
+struct PlaceholderView: View {
     var body: some View {
-        Text(entry.date, style: .time)
+        EmojiView(
+            emoji: Emoji(icon: "😊", name: "N/A", description: "N/A")
+        )
     }
 }
 
-@main
-struct My_Widget: Widget {
-    let kind: String = "My_Widget"
+struct WidgetEntryView: View {
+    let entry: Provider.Entry
+    
+    var body: some View {
+        EmojiView(emoji: entry.emoji)
+    }
+}
 
+@main //to the extension knows that this is main function
+struct MyWidget: Widget {
+    private let kind = "My_Widget"
+    
     var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            My_WidgetEntryView(entry: entry)
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            WidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
-    }
-}
-
-struct My_Widget_Previews: PreviewProvider {
-    static var previews: some View {
-        My_WidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
